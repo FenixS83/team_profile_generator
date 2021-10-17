8c7b92799701fc0e9b487edc580235c033df8cab
Fenix Sampson <Fenix.sampson@gmail.com>
2021-10-16 21:16:29 -0400

    added questions for engineer and intern

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const Manager = require("./lib/Manager");
 // const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const fs = require("fs");
-const {managerQuestions, internQuestions} = require("./src/questions");
+const {managerQuestions, engineerQuestions, internQuestions} = require("./src/questions");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 
 menu = () => {
@@ -14,10 +16,27 @@ menu = () => {
    const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
    console.log(manager)
 
-    
-    
     };
     createManager();
+
+
+
+    createEngineer = async () => {
+    const answers = await inquirer.prompt(engineerQuestions);
+    console.log(answers);
+    const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+    console.log(engineer)
+    };
+    createEngineer();
+
+
+    createIntern = async () => {
+    const answers = await inquirer.prompt(internQuestions);
+    console.log(answers);
+    const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+    console.log(intern)
+    };
+    createIntern()
 };
 
 
--- src/questions.js
+++ src/questions.js
@@ -6,6 +6,7 @@ managerQuestions : [
         name: "name",
         message: "What is the team manager's name?",
         // Validation here npm documentation npmjs.com
+        
     },
 
     {
@@ -28,7 +29,73 @@ managerQuestions : [
         message: "What is the team manager's office number?",
         // Validation here
     }
+],
+
+engineerQuestions : [
+    {
+        type:"input",
+        name: "name",
+        message: "What is the engineer's name?",
+        // Validation here npm documentation npmjs.com
+        
+    },
+
+    {
+        type: "input",
+        name: "id",
+        message: "What is the engineer's id?"
+        // Validation here
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email?",
+        // Validation here
+    },
+
+    {
+        type: "input",
+        name: "github",
+        message: "What is the engineer's GitHub username?",
+        // Validation here
+    }
+],
+
+internQuestions : [
+    {
+        type:"input",
+        name: "name",
+        message: "What is the intern's name?",
+        // Validation here npm documentation npmjs.com
+        
+    },
+
+    {
+        type: "input",
+        name: "id",
+        message: "What is the intern's id?"
+        // Validation here
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "What is the intern's email?",
+        // Validation here
+    },
+
+    {
+        type: "input",
+        name: "school",
+        message: "What school did the intern attend?",
+        // Validation here
+    }
 ]
+
+
+
+
 
 
 }
