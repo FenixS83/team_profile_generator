d48c54e019d4dbe7ebc1dc25dde4193c1c29f372
Fenix Sampson <Fenix.sampson@gmail.com>
2021-10-17 17:39:15 -0400

    cleaned up html files

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,18 +1,43 @@
-const Employee = require("./lib/Employee");
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const fs = require("fs");
-const {employeeQuestions, managerQuestions, engineerQuestions, internQuestions} = require("./src/questions");
+class generateHTML {
+    template = (users) => {
+    return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+    <title>Team Portfolio Generator</title>
+</head>
+<body>
+    <div class="jumbotron text-white bg-danger" style="min-height: 150px;">
+        <div class="container">
+            <h1 class="display-4 text-center align-middle">
+                My Team
+            </h1>
+        </div>
+    </div>
 
+    <div class"container">
+        <div class="row">
+            ${users}
+        </div>
+    </div>
 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-const generateHTML = ({name, id, email, role, officeNumber, github, school}) =>
-
+</body>
+</html>
 `
+    }
+}
 
-`;
+module.exports = generateHTML;
+
 
 
 .then((answers) => {
--- src/generator.js
+++ src/generator.js
@@ -1,46 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="./style.css">
-    <title>Team Portfolio Generator</title>
-</head>
-<body>
-    <div class="jumbotron text-white bg-danger" style="min-height: 150px;">
-        <div class="container">
-            <h1 class="display-4 text-center align-middle">
-                My Team
-            </h1>
-        </div>
-    </div>
-
-    <div class"container">
-        <div class="row">
-            ${users}
-        </div>
-    </div>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-</body>
-</html>
-
-
-
-
-
-
-
-
-
-
-    <div class="row row-cols-1 row-cols-md-3 g-5 m-5 justify-content-around">
-        <!-- Manager Card -->
+class Generator {
+    managerGenerator = (user) => {
+        return `
         <div class="col">
             <div class="card shadow-lg p-3 mb-5 border-dark" style="width: 18rem;">
                 <div class="card-header text-white bg-primary mb-3">
@@ -56,9 +16,12 @@
                     </ul>
                 </div>
             </div>
-        </div>
+        </div>       
+        `
+    }
 
-        <!-- Engineer Card -->
+    engineerGenerator = (user) => {
+        return `
         <div class="col">
             <div class="card shadow-lg p-3 mb-5 border-dark" style="width: 18rem;">
                 <div class="card-header text-white bg-primary mb-3">
@@ -75,8 +38,11 @@
                 </div>
             </div>
         </div>
+        `
+    }
 
-        <!-- Intern card -->
+    internGenerator = (user) => {
+        return `
         <div class="col">
             <div class="card shadow-lg p-3 mb-5 border-dark" style="width: 18rem;">
                 <div class="card-header text-white bg-primary mb-3">
@@ -93,7 +59,8 @@
                 </div>
             </div>
         </div>
+        `
+    }
+}
 
-
-    </div>
-    
+module.exports = Generator;
